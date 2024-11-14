37334e2fd73759102ccd29b0565c004c5dd54bb3
LakioLive <mr.lakio.sergii@gmail.com>
2024-11-14 20:39:44 +0100

    Edited the code and fixed bugs

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,25 +1,43 @@
-const showDropdown = (id) => {
-    const dropdown = document.querySelector(`#${id}`);
-
-    if (window.innerWidth > 800 && dropdown.classList.contains("hidden")) {
+const showDropdown = (dropdown) => {
+    if (dropdown.classList.contains("hidden")) {
         dropdown.classList.remove("hidden");
     }
 };
 
-const hideDropdown = (id) => {
-    const dropdown = document.querySelector(`#${id}`);
-
+const hideDropdown = (dropdown) => {
     if (!dropdown.classList.contains("hidden")) {
         dropdown.classList.add("hidden");
     }
 };
 
-const clickDropdown = (id) => {
-    const dropdown = document.querySelector(`#${id}`);
-
-    if (!dropdown.classList.contains("hidden")) {
-        dropdown.classList.add("hidden");
-    } else if (dropdown.classList.contains("hidden")) {
-        dropdown.classList.remove("hidden");
-    }
+const clickDropdown = (dropdown) => {
+    dropdown.classList.toggle("hidden");
 };
+
+const btnsDropdown = document.querySelectorAll(".btns_download");
+const dropdowns = document.querySelectorAll(".dropdown_card");
+
+btnsDropdown.forEach((btn, i) => {
+    const dropdown = dropdowns[i];
+
+    if (window.innerWidth > 800) {
+        btn.addEventListener("mouseenter", () => showDropdown(dropdown));
+        btn.addEventListener("mouseleave", () => hideDropdown(dropdown));
+    }
+
+    btn.addEventListener("click", () => clickDropdown(dropdown));
+    window.addEventListener("click", (e) => {
+        if (!e.target.closest(".btns_download")) {
+            hideDropdown(dropdown);
+        }
+    });
+});
+
+dropdowns.forEach((dropdown) => {
+    if (window.innerWidth > 800) {
+        dropdown.addEventListener("mouseenter", () => showDropdown(dropdown));
+        dropdown.addEventListener("mouseleave", () => hideDropdown(dropdown));
+    }
+
+    dropdown.addEventListener("click", () => clickDropdown(dropdown));
+});
